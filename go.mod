a81b7be05f8d1e46734f1ccd4a493f4201be04c6
Fabien Couillard <couillard.fabien+dev@gmail.com>
2024-03-31 11:51:48 +0200

    wip

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
-module github.com/microsoft/vscode-remote-try-go
+module trygo
 
-go 1.19
+go 1.22.1
+
+require (
+	github.com/abema/go-mp4 v1.2.0
+	github.com/sunfish-shogi/bufseekio v0.0.0-20210207115823-a4185644b365
+)
+
+require (
+	github.com/AlexEidt/Vidio v1.5.1 // indirect
+	github.com/google/uuid v1.1.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"fmt"
+	vidio "github.com/AlexEidt/Vidio"
+	"io/fs"
+	"log"
+	"os"
+	"slices"
+	"strings"
+	"sync"
+)
+
+type CMap = struct {
+	sync.RWMutex
+	value map[uint][]string
+}
+type CSet = struct {
+	sync.RWMutex
+	value map[uint]void
+}
+
+type void struct{}
+
+var member void
+
+func main() {
+	//base := "/home/fabien/Videos"
+	base := "/media/fabien/exdata/A1_over60"
+	result := CMap{value: make(map[uint][]string)}
+	keys := CSet{value: make(map[uint]void)}
+
+	ops := 0
+	var wg sync.WaitGroup
+
+	files, err := os.ReadDir("/home/fabien/Videos")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filesSlices := chunkSlice(files, 4)
+
+	for _, filesSlice := range filesSlices {
+		for _, file := range filesSlice {
+			ops++
+			if !file.IsDir() {
+				wg.Add(1)
+				read(base, file, ops, &wg, &result, &keys)
+			}
+		}
+	}
+
+	wg.Wait()
+
+	result.RLock()
+	keys.RLock()
+
+	list := make([]uint, 0, len(keys.value))
+	for k := range keys.value {
+		list = append(list, k)
+	}
+
+	slices.Sort(list)
+
+	for _, k := range list {
+		i := len(result.value[k])
+		//
+		if i > 1 {
+			fmt.Printf("%d :\n", k)
+			for _, f := range result.value[k] {
+				fmt.Printf("%v\n", f)
+				split := strings.Split(f, "/")
+				destPath := base + "/verify/" + split[len(split)-1]
+				fmt.Printf("will mote to %v\n", destPath)
+				err = os.Rename(f, destPath)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+
+		}
+	}
+
+	//fmt.Printf("result : %v", list)
+
+	//fmt.Printf("result : %v", result.value)
+	keys.RUnlock()
+	result.RUnlock()
+	//err := filepath.Walk("/home/fabien/Videos",
+	//	func(path string, info os.FileInfo, err error) error {
+	//	)}
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+}
+
+func read(base string, file fs.DirEntry, ops int, wg *sync.WaitGroup, result *CMap, keys *CSet) {
+	path := base + "/" + file.Name()
+	fmt.Printf("%d, @@@ path : %s\n", ops, path)
+	video, err := vidio.NewVideo(path)
+	if err != nil {
+		fmt.Printf("ERROR : %s", err)
+	}
+
+	duration := video.Duration()
+	durationAsInt := uint(duration)
+	fmt.Printf("lengh : %f or %d \n", duration, durationAsInt)
+
+	keys.Lock()
+	result.Lock()
+
+	keys.value[durationAsInt] = member
+	result.value[durationAsInt] = append(result.value[durationAsInt], path)
+
+	result.Unlock()
+	keys.Unlock()
+
+	wg.Done()
+}
+
+func chunkSlice(slice []fs.DirEntry, chunkSize int) [][]fs.DirEntry {
+	var chunks [][]fs.DirEntry
+	for i := 0; i < len(slice); i += chunkSize {
+		end := i + chunkSize
+
+		// necessary check to avoid slicing beyond
+		// slice capacity
+		if end > len(slice) {
+			end = len(slice)
+		}
+
+		chunks = append(chunks, slice[i:end])
+	}
+
+	return chunks
+}
